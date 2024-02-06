0651fc3e28ce02b20d1ce785eb2ed300ad1f0b0e
jeremy <78951628+bumpadumpt@users.noreply.github.com>
2024-02-06 11:53:18 -0800

    finished task 4

--- src/Board.java
+++ src/Board.java
@@ -1,25 +1,98 @@
 public class Board {
     // declare instance variables here!
+    int x;
+    int y;
+    private int[][] pieces;
     public Board(int x, int y) { // constructor!
-
+        this.x = x;
+        this.y = y;
+        pieces = new int[x][y];
+        for(int i = 0 ; i < x ; i++) {
+            for(int j = 0 ; j < y ; j++) {
+                pieces[i][j] = 0;
+            }
+        }
     }
     public void placePiece(Piece p) { // updates the state of the board with the information of the Piece
         if (p instanceof MegaPiece) {
             // hmm
         } else {
-            // hmm
+            pieces[p.getCol()][p.getRow()] = p.getColor();
         }
     }
 
 
     public int[][] getBoardPieces() { // getter function for boardPieces array
-        return null;
+        return pieces;
     }
 
     public void displayAll() { // displays the entire board
         // that's quite a nice display we've got right now (displays nothing)
+        System.out.println(" ");
+        for(int i = 0 ; i < pieces.length ; i ++) {
+            System.out.print(i);
+        }
+        for(int i = 0 ; i < pieces[0].length ; i ++) {
+            System.out.println("#");
+            for(int j = 0 ; j < pieces.length ; j ++) {
+                System.out.print(".");
+            }
+            System.out.print("#");
+        }
+        for(int i = 0 ; i < pieces.length + 2 ; i ++) {
+            System.out.print("#");
+        }
     }
     public boolean checkForWin(int player) { //FIXME
+        String checkVert = "";
+        for (int[] piece : pieces) {
+            checkVert = "";
+            for (int j = 0; j < pieces[0].length; j++) {
+                checkVert += piece[j];
+            }
+            if(player == 1) {
+                if (checkVert.contains("11111")) {
+                    return true;
+                }
+            } else {
+                if (checkVert.contains("22222")) {
+                    return true;
+                }
+            }
+        }
+        String checkHorz = "";
+        for(int i = 0 ; i < pieces[0].length ; i++) {
+            checkHorz = "";
+            for (int[] piece : pieces) {
+                checkHorz += piece[i];
+            }
+            if(player == 1) {
+                if (checkHorz.contains("11111")) {
+                    return true;
+                }
+            } else {
+                if (checkHorz.contains("22222")) {
+                    return true;
+                }
+            }
+        }
+        String checkDiag = "";
+        for(int i = 5 ; i < pieces.length ; i++) {
+            checkDiag = "";
+            for (int j = 0 ; j < pieces.length ; j ++) {
+                checkDiag += pieces[i-j][j];
+            }
+            if(player == 1) {
+                if (checkDiag.contains("11111")) {
+                    return true;
+                }
+            } else {
+                if (checkDiag.contains("22222")) {
+                    return true;
+                }
+            }
+        }
+
         return false;
     }
 
--- src/Piece.java
+++ src/Piece.java
@@ -1,7 +1,11 @@
 public class Piece {
     // instance variables go here!
+    private int row, color, column;
 
     public Piece(int col, int color, Board board) { // constructor!
+        this.color = color;
+        this.column = col;
+        row = findRow(column, board);
 
     }
 
@@ -10,16 +14,23 @@ public class Piece {
         if (this instanceof MegaPiece) {
             return 0;
         } else {
+            for(int i = boardPieces[column].length-1 ; i >= 0 ; i--) {
+                System.out.println(boardPieces[column][i]);
+                if(boardPieces[column][i] == 0) {
+                    boardPieces[column][i] = color;
+                    return i;
+                }
+            }
             return 0;
         }
     }
     public int getCol() {
-        return 0;
+        return column;
     } //FIXME
     public int getRow() {
-        return 0;
+        return row;
     }
     public int getColor() {
-        return 0;
+        return color;
     }
 }
